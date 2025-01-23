860e2f049da2fbf9e41caa10a095aaf139af07b5
Mcnafaha <jafetguzmanflores@gmail.com>
2025-01-23 17:34:47 -0500

    fix(app/layout): correctly adds nunito font (i think)
    plus merge conflict so much fun

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,20 @@
 import { type Config } from "tailwindcss";
 
-
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)'],
-        mono: ['var(--font-roboto-mono)'],
+        sans: ["var(--font-nunito)"],
       },
     },
     colors: {
-        "dark_blue": "hsl(209, 23%, 22%)",
-        "very_dark_blue" : "hsl(207, 26%, 17%)",
-        "very_dark_blue_light_mode": "hsl(200, 15%, 8%)",
-        "dark_gray_light_mode_input": "hsl(0, 0%, 52%)",
-        "very_light_gray_lightmodebg" : "hsl(0, 0%, 98%)",
-        "white_Dark_Modetxt_lightmodeEl" : "hsl(0, 0%, 100%)",
+      "dark_blue": "hsl(209, 23%, 22%)",
+      "very_dark_blue": "hsl(207, 26%, 17%)",
+      "very_dark_blue_light_mode": "hsl(200, 15%, 8%)",
+      "dark_gray_light_mode_input": "hsl(0, 0%, 52%)",
+      "very_light_gray_lightmodebg": "hsl(0, 0%, 98%)",
+      "white_Dark_Modetxt_lightmodeEl": "hsl(0, 0%, 100%)",
     }
   },
   plugins: [],
