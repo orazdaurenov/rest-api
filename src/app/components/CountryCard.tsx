380ae509bbcbf87a0bc2a298316493163f50d37c
orazbek <oraz.daurenov@gmail.com>
2025-01-24 23:43:23 +0100

    merged FF & Style for article

--- src/app/components/CountryCard.tsx
+++ src/app/components/CountryCard.tsx
@@ -7,12 +7,14 @@ type CardProps = {
 
 const CountryCard = ({ country }: CardProps) => {
   return (
-    <article className="border-white text-black h-48 w-40 border border-solid">
-      <img className="h-20 w-full" src={country.flags.svg} alt={country.name} />
-      <h3>{country.name}</h3>
-      <p>Popultation: {country.population}</p>
-      <p>Region: {country.region}</p>
-      <p>Capital: {country.capital}</p>
+    <article className="text-black bg-white">
+      <img className="h-24 w-max" src={country.flags.svg} alt={country.name} />
+      <div className="px-2 pb-2 pt-0">
+        <h3>{country.name}</h3>
+        <p>Popultation: {country.population}</p>
+        <p>Region: {country.region}</p>
+        <p>Capital: {country.capital}</p>
+      </div>
     </article>
   );
 };
