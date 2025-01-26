d46a3e9ca553b4bebfc2359b31e67f44268563a0
orazbek <oraz.daurenov@gmail.com>
2025-01-26 23:26:23 +0100

    Input Filter defined and works

--- src/app/components/FilterBar.tsx
+++ src/app/components/FilterBar.tsx
@@ -8,58 +8,82 @@ type FilterProps = {
 };
 
 const FilterBar = ({ countries, setCountries }: FilterProps) => {
+  return (
+    <div className="my-5 flex justify-between px-0">
+      <InputFilter setCountries={setCountries} countries={countries} />
+      <SelectFilter countries={countries} setCountries={setCountries} />
+    </div>
+  );
+};
+
+const InputFilter = ({ countries, setCountries }: FilterProps) => {
+  const [curCountries, setFilteredCountries] = useState("");
+  const onSearch = () => {
+    const allCountries = countries;
+    const countryArr = [];
+    //countryarr
+    //INSIDE LOOP
+    // if(has string) => push into country arr
+    //END LOOP
+    // setState(countryArr)
+    for (let index = 0; index < allCountries.length; index++) {
+      const currentItem = allCountries[index];
+      if (currentItem?.name.includes(curCountries)) {
+        countryArr.push(currentItem);
+      }
+    }
+    setCountries(countryArr);
+  };
+
+  return (
+    <>
+      <input
+        className="rounded-md border border-gray-200 p-2 text-gray-500"
+        type="text"
+        value={curCountries}
+        placeholder="Search for a country..."
+        onChange={(e) => {
+          setFilteredCountries(e.target.value);
+          onSearch();
+        }}
+      />
+    </>
+  );
+};
+
+const SelectFilter = ({ countries, setCountries }: FilterProps) => {
   const [definedRegion, setDefinedRegion] = useState(false);
   const allRegions = countries.map((c) => c.region);
   const IndRegions = new Set(allRegions);
   const SelectedRegions = Array.from(IndRegions);
 
   return (
-    <div className="my-5 flex justify-between px-0">
-      <input
-        className="rounded-md border border-gray-200 p-2 text-gray-500"
-        type="text"
-        value=""
-        placeholder="Search for a country..."
-      />
-      <select
-        className="rounded-md border border-gray-200 p-2 text-gray-500"
-        name=""
-        id=""
-        onChange={(e) => {
-          const region = e.target.value;
-          if (region === "reset") {
-            setCountries(countries);
-            return;
-          }
-          const filteredRegions = countries.filter(
-            (country) => country.region === region,
-          );
-          setCountries(filteredRegions);
-          setDefinedRegion(true);
-
-          // if (region === "Europe") {
-          //   const europeanNations = countries.filter(
-          //     (country) => country.region === "Europe",
-          //   );
-          //   setCountries(europeanNations);
-          // } else if (region === "Asia") {
-          //   const asianNations = countries.filter(
-          //     (country) => country.region === "Asia",
-          //   );
-          //   setCountries(asianNations);
-          // }
-        }}
-      >
-        <option value="reset">
-          {definedRegion ? "All" : "Filter by Region"}
+    <select
+      className="rounded-md border border-gray-200 p-2 text-gray-500"
+      name=""
+      id=""
+      onChange={(e) => {
+        const region = e.target.value;
+        if (region === "reset") {
+          setCountries(countries);
+          return;
+        }
+        const filteredRegions = countries.filter(
+          (country) => country.region === region,
+        );
+        setCountries(filteredRegions);
+        setDefinedRegion(true);
+      }}
+    >
+      <option value="reset">
+        {definedRegion ? "All" : "Filter by Region"}
+      </option>
+      {SelectedRegions.map((region) => (
+        <option key={region} value={region}>
+          {region}
         </option>
-        {SelectedRegions.map((region) => (
-          <option key={region} value={region}>
-            {region}
-          </option>
-        ))}
-      </select>
-    </div>
+      ))}
+    </select>
   );
 };
 
