ab13d172793b376d19a0e44488e648a9ccc4f2d5
orazbek <oraz.daurenov@gmail.com>
2025-01-28 22:33:05 +0100

    main page anchor btn inside navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const NavBar = () => {
   return (
     <div className="m-0 flex justify-between border-b-2 border-b-gray-200 py-5">
-      <h1 className="text-lg font-bold">Where in the world ?</h1>
+      <h1 className="text-lg font-bold">
+        <a href="/countries/">Where in the world ?</a>
+      </h1>
       <p>Dark Mode</p>
     </div>
   );
