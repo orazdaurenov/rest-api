cd865cb5f29dab09ea8e7d654c0776a50b960ea0
orazbek <oraz.daurenov@gmail.com>
2025-01-31 00:14:10 +0100

    fixed next config file

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { NextConfig } from "next";
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
-
+const config: NextConfig = {
+  images: {
+    domains: ['upload.wikimedia.org'],  // Allow this domain
+  },
+};
 export default config;
